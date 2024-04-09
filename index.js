ee006d502aebe60b1eeecf5c26e093d0cf6dda85
SeHawn <hansae167@naver.com>
2024-04-10 00:41:00 +0900

    install and branch

--- index.js
+++ index.js
@@ -123,8 +123,8 @@ server.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 
 });
-
+/*
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
-});
+}); */});
