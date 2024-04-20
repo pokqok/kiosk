b0750c87fbc7b0ccafd33bcd85b212586d1f7c4e
Jeonghoonchoi74 <wjdgnsdldyd74@gmail.com>
2024-04-20 22:54:30 +0900

    merge finished

--- dto/categorys.js
+++ dto/categorys.js
@@ -1,8 +1,8 @@
 // 카테고리 설정 쿼리 파일
-const mariadb = require('mariadb');
-const express = require('express');
+const mariadb = require("mariadb");
+const express = require("express");
 const router = express.Router();
-const config = require('../DBconfig.json');
+const config = require("../DBconfig.json");
 /*
 
 
@@ -18,146 +18,156 @@ let transaction; // 공유되는 트랜잭션임
 //이걸로 apply/cancel해야 거기서 commit/rollback을 해줌
 
 // DB 가져오기
-router.get('/getAllCategories', async (req, res) => {
+router.get("/getAllCategories", async (req, res) => {
   try {
     const conn = await pool.getConnection();
-    const result = await conn.query('SELECT * FROM category');
+    const result = await conn.query("SELECT * FROM CATEGORY");
     conn.release();
-    console.log('가져오기 성공');
+    console.log("가져오기 성공");
     res.status(200).json(result);
   } catch (error) {
-    console.error('카테고리 가져오기 에러:', error);
-    res.status(500).send('카테고리 가져오기 실패');
+    console.error("카테고리 가져오기 에러:", error);
+    res.status(500).send("카테고리 가져오기 실패");
   }
 });
 
 // 태그 추가
-router.post('/add', async (req, res) => {
+router.post("/add", async (req, res) => {
   try {
     const { categoryId, categoryName } = req.body;
-    if (!transaction) { //트랜잭션이 비어있음 여기서 start transaction함
+    if (!transaction) {
+      //트랜잭션이 비어있음 여기서 start transaction함
       transaction = await pool.getConnection();
-      await transaction.query('START TRANSACTION');
-    }//만약 아니면 이미 있다는 것이니 이어서 실행
-    await transaction.query(`INSERT INTO category (CategoryNO, CategoryName, CategoryAlias) VALUES (${categoryId},'${categoryName}', NULL)`);
-    console.log('추가 성공');
-    res.status(200).send('카테고리 추가 성공');
+      await transaction.query("START TRANSACTION");
+    } //만약 아니면 이미 있다는 것이니 이어서 실행
+    await transaction.query(
+      `INSERT INTO CATEGORY (CategoryNO, CategoryName, CategoryAlias) VALUES (${categoryId},'${categoryName}', NULL)`
+    );
+    console.log("추가 성공");
+    res.status(200).send("카테고리 추가 성공");
   } catch (error) {
-    console.error('카테고리 추가 에러:', error);
+    console.error("카테고리 추가 에러:", error);
     if (transaction) {
       await transaction.rollback();
       transaction.release();
       transaction = undefined;
     }
-    res.status(500).send('카테고리 추가 실패');
+    res.status(500).send("카테고리 추가 실패");
   }
 });
 
 // 태그 삭제
-router.post('/delete', async (req, res) => {
+router.post("/delete", async (req, res) => {
   try {
     const { categoryId } = req.body;
-    if (!transaction) {//위와 동일하다.
+    if (!transaction) {
+      //위와 동일하다.
       transaction = await pool.getConnection();
-      await transaction.query('START TRANSACTION');
+      await transaction.query("START TRANSACTION");
     }
     //console.log('this is :' + categoryId);
-    await transaction.query(`DELETE FROM category WHERE CategoryNO = ${categoryId}`);
-    console.log('삭제 성공');
-    res.status(200).send('카테고리 삭제 성공');
+    await transaction.query(
+      `DELETE FROM CATEGORY WHERE CategoryNO = ${categoryId}`
+    );
+    console.log("삭제 성공");
+    res.status(200).send("카테고리 삭제 성공");
   } catch (error) {
-    console.error('카테고리 삭제 에러:', error);
+    console.error("카테고리 삭제 에러:", error);
     if (transaction) {
       await transaction.rollback();
       transaction.release();
       transaction = undefined;
     }
-    res.status(500).send('카테고리 삭제 실패');
+    res.status(500).send("카테고리 삭제 실패");
   }
 });
 
 // 태그 수정
-router.post('/update', async (req, res) => {
+router.post("/update", async (req, res) => {
   try {
-    const { categoryId, categoryName} = req.body;
+    const { categoryId, categoryName } = req.body;
     if (!transaction) {
       transaction = await pool.getConnection();
-      await transaction.query('START TRANSACTION');
+      await transaction.query("START TRANSACTION");
     }
-    await transaction.query(`UPDATE category SET CategoryName = '${categoryName}' WHERE CategoryNO = ${categoryId}`);
-    console.log('수정 성공');
-    res.status(200).send('카테고리 수정 성공');
+    await transaction.query(
+      `UPDATE CATEGORY SET CategoryName = '${categoryName}' WHERE CategoryNO = ${categoryId}`
+    );
+    console.log("수정 성공");
+    res.status(200).send("카테고리 수정 성공");
   } catch (error) {
-    console.error('카테고리 수정 에러:', error);
+    console.error("카테고리 수정 에러:", error);
     if (transaction) {
       await transaction.rollback();
       transaction.release();
       transaction = undefined;
     }
-    res.status(500).send('카테고리 수정 실패');
+    res.status(500).send("카테고리 수정 실패");
   }
 });
 
 //태그 on/off
-router.post('/toggle', async (req, res) => {
+router.post("/toggle", async (req, res) => {
   try {
     const { categoryId, categoryOn } = req.body;
     if (!transaction) {
       transaction = await pool.getConnection();
-      await transaction.query('START TRANSACTION');
+      await transaction.query("START TRANSACTION");
     }
     let switchOn;
-    categoryOn != categoryOn //이건 굳이 필요없..나? front에서는 v-switch가 자동으로 바꾸어주긴 하나 여긴 보낸 데이터라
-     //mariaDB는 bool 구문 인식을 1/0 으로 하기에 혹시몰라서 추가함 
-    if(categoryOn == true){
+    categoryOn != categoryOn; //이건 굳이 필요없..나? front에서는 v-switch가 자동으로 바꾸어주긴 하나 여긴 보낸 데이터라
+    //mariaDB는 bool 구문 인식을 1/0 으로 하기에 혹시몰라서 추가함
+    if (categoryOn == true) {
       switchOn = 1;
-    }else{
+    } else {
       switchOn = 0;
     }
     //console.log(categoryId.isOn);
     //아니 왜 categoryID만 보냈는데 이게 보내지는가?...했더니 하나로 뭉쳐서 보냈음
-    await transaction.query(`UPDATE category SET IsOn = ${switchOn} WHERE CategoryNO = ${categoryId}`);
-    
-    console.log('수정 성공');
-    res.status(200).send('카테고리 수정 성공');
+    await transaction.query(
+      `UPDATE CATEGORY SET IsOn = ${switchOn} WHERE CategoryNO = ${categoryId}`
+    );
+
+    console.log("수정 성공");
+    res.status(200).send("카테고리 수정 성공");
   } catch (error) {
-    console.error('카테고리 수정 에러:', error);
+    console.error("카테고리 수정 에러:", error);
     if (transaction) {
       await transaction.rollback();
       transaction.release();
       transaction = undefined;
     }
-    res.status(500).send('카테고리 수정 실패');
+    res.status(500).send("카테고리 수정 실패");
   }
 });
 
 // 변경 사항 적용
-router.post('/apply', async (req, res) => {
+router.post("/apply", async (req, res) => {
   try {
     if (transaction) {
       await transaction.commit();
       transaction.release();
       transaction = undefined;
-    }//트랜잭션 존재 시, 커밋하고 초기화
-    res.status(200).send('변경 사항이 성공적으로 적용되었습니다.');
+    } //트랜잭션 존재 시, 커밋하고 초기화
+    res.status(200).send("변경 사항이 성공적으로 적용되었습니다.");
   } catch (error) {
-    console.error('변경 사항 적용 에러:', error);
-    res.status(500).send('변경 사항을 적용하는 도중 에러가 발생했습니다.');
+    console.error("변경 사항 적용 에러:", error);
+    res.status(500).send("변경 사항을 적용하는 도중 에러가 발생했습니다.");
   }
 });
 
 // 변경 사항 취소
-router.post('/cancel', async (req, res) => {
+router.post("/cancel", async (req, res) => {
   try {
     if (transaction) {
       await transaction.rollback();
       transaction.release();
       transaction = undefined;
-    }//트랜잭션 존재 시, 롤백하고 초기화
-    res.status(200).send('변경 사항이 취소되었습니다.');
+    } //트랜잭션 존재 시, 롤백하고 초기화
+    res.status(200).send("변경 사항이 취소되었습니다.");
   } catch (error) {
-    console.error('변경 사항 취소 에러:', error);
-    res.status(500).send('변경 사항을 취소하는 도중 에러가 발생했습니다.');
+    console.error("변경 사항 취소 에러:", error);
+    res.status(500).send("변경 사항을 취소하는 도중 에러가 발생했습니다.");
   }
 });
 
--- frontend/src/tagModule.js
+++ frontend/src/tagModule.js
@@ -1,221 +1,223 @@
-import axios from 'axios';
+import axios from "axios";
 
+const tagModule = {
+  state() {
+    return {
+      tags: [],
+      // [{ id: 1, name: '태그 1' ,description: '설명', alias: 'tag1', isOn: falsem isRequired,},];
+    };
+  },
 
-const HOST = "http://localhost:3000";
-
-const tagModule = 
-{
-    state(){
-        return {
-            tags: [],
-            // [{ id: 1, name: '태그 1' ,description: '설명', alias: 'tag1', isOn: falsem isRequired,},];
-        }
+  mutations: {
+    //태그 표시 여부
+    toggleIsOn(state, TagId) {
+      const Tag = state.tags.find((cat) => cat.id === TagId);
+      if (Tag) {
+        let switchOn = Tag.isOn;
+        Tag.isOn = switchOn;
+        //바꾸지 않은 이유는, v-switch에 모델을 설정해주면, 이벤트 함수 설정 없이도 switch를 누르면 값이 바뀐다.
+      }
+    },
+    //필수 여부
+    toggleRequired(state, TagId) {
+      const Tag = state.tags.find((cat) => cat.id === TagId);
+      if (Tag) {
+        let switchOn = Tag.isRequired;
+        Tag.isRequired = switchOn;
+        //바꾸지 않은 이유는, v-switch에 모델을 설정해주면, 이벤트 함수 설정 없이도 switch를 누르면 값이 바뀐다.
+      }
+    },
+    //DB에서 태그 업데이트
+    setTags(state, Tags) {
+      state.tags = Tags; // 카테고리 데이터 업데이트
     },
 
-    mutations: {
-        //태그 표시 여부
-        toggleIsOn(state, TagId) {
-            const Tag = state.tags.find(cat => cat.id === TagId);
-            if (Tag) {
-              let switchOn = Tag.isOn;
-              Tag.isOn = switchOn;
-              //바꾸지 않은 이유는, v-switch에 모델을 설정해주면, 이벤트 함수 설정 없이도 switch를 누르면 값이 바뀐다.
-            }
-        },
-        //필수 여부
-        toggleRequired(state, TagId) {
-            const Tag = state.tags.find(cat => cat.id === TagId);
-            if (Tag) {
-              let switchOn = Tag.isRequired;
-              Tag.isRequired = switchOn;
-              //바꾸지 않은 이유는, v-switch에 모델을 설정해주면, 이벤트 함수 설정 없이도 switch를 누르면 값이 바뀐다.
-            }
-        },
-        //DB에서 태그 업데이트
-        setTags(state, Tags) {
-            state.tags = Tags; // 카테고리 데이터 업데이트 
-          },
-
-        //새로고침할 시,
-        changeTags(state, newTags) {
-            state.tags = newTags;
-        },
-
-        //태그 생성
-        addTag(state, newTag) {
-            if (newTag.name.trim() !== '') {
-                //+1하는 걸 vue에서 하도록 바꿈, 10단위로 올라가는 문제가 자꾸 생김
-                state.tags.push(newTag);
-              }
-          },
-        
-        //태그 변경
-        updateTag(state, {id, name, alias, description}){
-            const TagToUpdate = state.tags.find(Tag => Tag.id == id);
-            if (TagToUpdate) {
-              TagToUpdate.name = name;
-              TagToUpdate.alias = alias;
-              TagToUpdate.description = description;
-              //TagToUpdate.description = description;
-            }
-            //vue에서 사용법 (함수 내에서 )= this.$store.commit('updateTag', { id: 1, newName: '새로운 이름', newAlias: '새로운 별칭' });
-        },
-
-        //태그 삭제
-        deleteTag(state, id) {
-            const indexToDelete = state.tags.findIndex(Tag => Tag.id === id);
-            if (indexToDelete !== -1) {
-              // 선택한 id의 카테고리 삭제
-              state.tags.splice(indexToDelete, 1);
-            }
-        },
-      
-
-        //밑에 더 추가 바람
+    //새로고침할 시,
+    changeTags(state, newTags) {
+      state.tags = newTags;
     },
 
-    actions: {
-  
-        async fetchTags({ commit }) {
-            try {
-             
-              const response = await axios.get(HOST + '/tag/getTag');
-
-              let TagData = [];
-              // 응답에서 카테고리 데이터 추출
-              const responseData = response.data;
-
-              responseData.forEach(item => {
-                // 원하는 형태로 변환하여 객체 생성
-                const setTag = {
-                    id: item.TagNO,
-                    name: item.TagName,
-                    alias: item.TagAlias,
-                    description: item.TagDet,
-                    isRequired: item.IsRequired == 1 ? true : false,
-                    isOn: item.IsOn == 1 ? true : false
-                    //mariaDB는 1/0 으로 저장하기에 이렇게 지속적인 변경이 필요하다.
-                };
-                TagData.push(setTag);
-            });
-              // 받아온 카테고리 데이터를 저장하기 위해 mutation 호출
-              commit('setTags', TagData);
-            } catch (error) {
-              console.error('카테고리 데이터를 불러오는 중 오류 발생:', error);
-            }
-        },
-            //카테고리 추가
-        async addTag(context, newTag) {
-            try {
-                // 서버에 카테고리 추가 요청 보내기
-                await axios.post(HOST+'/tag/addTag', { TagId: newTag.id, TagName: newTag.name, TagAlias: newTag.alias, TagDescription: newTag.description });
-        
-                // DB에 추가하면 이제 여기 store(웹페이지)에서에 데이터 추가도 적용
-                console.log(newTag.name + '추가 성공');
-                context.commit('addTag', newTag);
-            } catch (error) {
-                console.error('카테고리 추가 중 오류 발생:', error);
-            }
-            //context.commit('addTag', newTag);
-          },
-    
-        async deleteTag(context, id) {
-            try {
-                // 서버에 카테고리 삭제 요청 보내기
-                alert('id: ' + id);
-                await axios.post(HOST+'/tag/deleteTag', { TagId: id });
-        
-                // DB에 추가하면 이제 여기 store에도 적용
-                context.commit('deleteTag', id);
-            } catch (error) {
-                console.error('카테고리 삭제 중 오류 발생:', error);
-            }
-            //context.commit('deleteTag', id);
-        },
-        
-
-        async updateTag(context, { id, name, alias, description}) {
-            try {
-        
-                //{ id: editedId, name: newName, alias: newAlias, description: newDescription}
-                alert('이름: ' + name + 'id: ' + id);
-                await axios.post(HOST+'/tag/updateTag', { TagId: id, TagName: name, TagAlias: alias, TagDescrption: description});
-        
-                // 변경 성공 시 스토어 상태 업데이트
-                context.commit('updateTag', {id, name, alias, description});
-            } catch (error) {
-                console.error('카테고리 변경 중 오류 발생:', error);
-            }
-           // context.commit('updateTag', { id, newName, newAlias });
-        },
-        
-          // 카테고리 적용, 
-        async applyTag(context, newTags) {
-            // 카테고리 데이터 교체 뮤테이션 호출
-            try {
-                // 서버에 카테고리 업데이트 요청 보내기
-                await axios.post(HOST+'/tag/apply', newTags);
-                // 업데이트 성공 시 스토어 상태 업데이트
-                // 다시 가져오도록 하기 (사실 굳이 상관은 없다)
-                context.commit('changeTags', newTags);
-            } catch (error) {
-                console.error('카테고리 업데이트 중 오류 발생:', error);
-            }
-            //context.commit('applyTag', newTags);
-        },
-        
-        //카테고리 적용 취소, 다 rollback하고 다시 DB에서 가져온다.
-        async cancelTag(context) {
-            try {
-                // 서버에 카테고리 취소 요청 보내기
-                await axios.post(HOST+'/tag/cancel');
-                // 취소 성공 시 다시 DB에서 가져와서 초기화하기
-                context.dispatch('fetchTags');
-            } catch (error) {
-                console.error('카테고리 취소 중 오류 발생:', error);
-            }
-        },
-
-        async toggleIsOn(context, {id, isOn}) {
-            try {
-                // 서버에 카테고리 취소 요청 보내기
-                console.log('toggleIsOn action called with id:', id, 'isOn:', isOn);
-
-                await axios.post(HOST+'/tag/toggleIsOn', {TagId: id, TagOn: isOn} );
-                // 취소 성공 시 다시 DB에서 가져와서 초기화하기
-                
-                context.commit('toggleIsOn', id);
-            } catch (error) {
-                console.error('표시 스위칭 중 오류 발생:', error);
-            }
-        },
-
-        async toggleRequired(context, {id, isRequired}) {
-            try {
-                // 서버에 카테고리 취소 요청 보내기
-                console.log(isRequired);
-                await axios.post(HOST+'/tag/toggleRequired', {TagId: id, TagOn: isRequired} );
-                // 취소 성공 시 다시 DB에서 가져와서 초기화하기
-                context.commit('toggleRequired', id);
-            } catch (error) {
-                console.error('필수 스위칭 중 오류 발생:', error);
-            }
-        },
+    //태그 생성
+    addTag(state, newTag) {
+      if (newTag.name.trim() !== "") {
+        //+1하는 걸 vue에서 하도록 바꿈, 10단위로 올라가는 문제가 자꾸 생김
+        state.tags.push(newTag);
+      }
     },
 
-    getters: {
-        // 모든 카테고리 반환
-        allTags: state => state.tags,
+    //태그 변경
+    updateTag(state, { id, name, alias, description }) {
+      const TagToUpdate = state.tags.find((Tag) => Tag.id == id);
+      if (TagToUpdate) {
+        TagToUpdate.name = name;
+        TagToUpdate.alias = alias;
+        TagToUpdate.description = description;
+        //TagToUpdate.description = description;
+      }
+      //vue에서 사용법 (함수 내에서 )= this.$store.commit('updateTag', { id: 1, newName: '새로운 이름', newAlias: '새로운 별칭' });
+    },
 
-        /*
+    //태그 삭제
+    deleteTag(state, id) {
+      const indexToDelete = state.tags.findIndex((Tag) => Tag.id === id);
+      if (indexToDelete !== -1) {
+        // 선택한 id의 카테고리 삭제
+        state.tags.splice(indexToDelete, 1);
+      }
+    },
+
+    //밑에 더 추가 바람
+  },
+
+  actions: {
+    async fetchTags({ commit }) {
+      try {
+        const response = await axios.get("/tag/getTag");
+
+        let TagData = [];
+        // 응답에서 카테고리 데이터 추출
+        const responseData = response.data;
+
+        responseData.forEach((item) => {
+          // 원하는 형태로 변환하여 객체 생성
+          const setTag = {
+            id: item.TagNO,
+            name: item.TagName,
+            alias: item.TagAlias,
+            description: item.TagDet,
+            isRequired: item.IsRequired == 1 ? true : false,
+            isOn: item.IsOn == 1 ? true : false,
+            //mariaDB는 1/0 으로 저장하기에 이렇게 지속적인 변경이 필요하다.
+          };
+          TagData.push(setTag);
+        });
+        // 받아온 카테고리 데이터를 저장하기 위해 mutation 호출
+        commit("setTags", TagData);
+      } catch (error) {
+        console.error("카테고리 데이터를 불러오는 중 오류 발생:", error);
+      }
+    },
+    //카테고리 추가
+    async addTag(context, newTag) {
+      try {
+        // 서버에 카테고리 추가 요청 보내기
+        await axios.post("/tag/addTag", {
+          TagId: newTag.id,
+          TagName: newTag.name,
+          TagAlias: newTag.alias,
+          TagDescription: newTag.description,
+        });
+
+        // DB에 추가하면 이제 여기 store(웹페이지)에서에 데이터 추가도 적용
+        console.log(newTag.name + "추가 성공");
+        context.commit("addTag", newTag);
+      } catch (error) {
+        console.error("카테고리 추가 중 오류 발생:", error);
+      }
+      //context.commit('addTag', newTag);
+    },
+
+    async deleteTag(context, id) {
+      try {
+        // 서버에 카테고리 삭제 요청 보내기
+        alert("id: " + id);
+        await axios.post("/tag/deleteTag", { TagId: id });
+
+        // DB에 추가하면 이제 여기 store에도 적용
+        context.commit("deleteTag", id);
+      } catch (error) {
+        console.error("카테고리 삭제 중 오류 발생:", error);
+      }
+      //context.commit('deleteTag', id);
+    },
+
+    async updateTag(context, { id, name, alias, description }) {
+      try {
+        //{ id: editedId, name: newName, alias: newAlias, description: newDescription}
+        alert("이름: " + name + "id: " + id);
+        await axios.post("/tag/updateTag", {
+          TagId: id,
+          TagName: name,
+          TagAlias: alias,
+          TagDescrption: description,
+        });
+
+        // 변경 성공 시 스토어 상태 업데이트
+        context.commit("updateTag", { id, name, alias, description });
+      } catch (error) {
+        console.error("카테고리 변경 중 오류 발생:", error);
+      }
+      // context.commit('updateTag', { id, newName, newAlias });
+    },
+
+    // 카테고리 적용,
+    async applyTag(context, newTags) {
+      // 카테고리 데이터 교체 뮤테이션 호출
+      try {
+        // 서버에 카테고리 업데이트 요청 보내기
+        await axios.post("/tag/apply", newTags);
+        // 업데이트 성공 시 스토어 상태 업데이트
+        // 다시 가져오도록 하기 (사실 굳이 상관은 없다)
+        context.commit("changeTags", newTags);
+      } catch (error) {
+        console.error("카테고리 업데이트 중 오류 발생:", error);
+      }
+      //context.commit('applyTag', newTags);
+    },
+
+    //카테고리 적용 취소, 다 rollback하고 다시 DB에서 가져온다.
+    async cancelTag(context) {
+      try {
+        // 서버에 카테고리 취소 요청 보내기
+        await axios.post("/tag/cancel");
+        // 취소 성공 시 다시 DB에서 가져와서 초기화하기
+        context.dispatch("fetchTags");
+      } catch (error) {
+        console.error("카테고리 취소 중 오류 발생:", error);
+      }
+    },
+
+    async toggleIsOn(context, { id, isOn }) {
+      try {
+        // 서버에 카테고리 취소 요청 보내기
+        console.log("toggleIsOn action called with id:", id, "isOn:", isOn);
+
+        await axios.post("/tag/toggleIsOn", { TagId: id, TagOn: isOn });
+        // 취소 성공 시 다시 DB에서 가져와서 초기화하기
+
+        context.commit("toggleIsOn", id);
+      } catch (error) {
+        console.error("표시 스위칭 중 오류 발생:", error);
+      }
+    },
+
+    async toggleRequired(context, { id, isRequired }) {
+      try {
+        // 서버에 카테고리 취소 요청 보내기
+        console.log(isRequired);
+        await axios.post("/tag/toggleRequired", {
+          TagId: id,
+          TagOn: isRequired,
+        });
+        // 취소 성공 시 다시 DB에서 가져와서 초기화하기
+        context.commit("toggleRequired", id);
+      } catch (error) {
+        console.error("필수 스위칭 중 오류 발생:", error);
+      }
+    },
+  },
+
+  getters: {
+    // 모든 카테고리 반환
+    allTags: (state) => state.tags,
+
+    /*
         // 특정 이름을 가진 카테고리 반환
         getTagByName: (state) => (name) => {
             return state.tags.find(Tag => Tag.name === name);
         },
         */
-
-    },
-
-}
+  },
+};
 
 export default tagModule;
