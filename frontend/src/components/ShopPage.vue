44223fa7f483a8f5ffbd76f568c2655643a352c6
pokqok <qamac323@gmail.com>
2024-05-26 17:05:26 +0900

    play오류 수정

--- frontend/src/components/ShopPage.vue
+++ frontend/src/components/ShopPage.vue
@@ -17,16 +17,6 @@
       </v-col>
       <template v-slot:extension>
         <v-tabs v-model="tab" grow>
-          <!-- db 연결 시 -->
-          <!-- <v-tab
-            v-for="i in categories"
-            :key="i"
-            :href="'#CategoryTitle' + i.name"
-          >
-            Category {{ i.name }}
-          </v-tab> -->
-
-          <!-- testdata 사용 시 -->
           <v-tab
             v-for="i in testCategory"
             :key="i"
@@ -40,16 +30,11 @@
   </div>
 
   <div style="margin-top: 140px"></div>
-  <!-- db연결 시 -->
-  <!-- <v-container v-for="i in categories" :key="i"> -->
-
-  <!-- testdata 사용 시 -->
   <v-container v-for="i in testCategory" :key="i">
     <h4 style="scroll-margin: 140px" :id="'CategoryTitle' + i.name">
       #{{ i.name }}
     </h4>
     <v-row>
-      <!-- test, 나중에 testdata대신 카테고리에 있는 메뉴목록 -->
       <v-col cols="4" v-for="item in filteredProducts(i.id)" :key="item.id">
         <ProductItem
           :product="item"
@@ -60,7 +45,6 @@
     <v-divider class="my-5"></v-divider>
   </v-container>
 
-  <!-- 이부분에 테스트 데이터는 아래 두개 filtered 메서드 부분에서 변경해줘야함 -->
   <ProductOptionModal
     @closeProductOptionModal="closeProductOptionModal"
     @payment="payment"
@@ -99,8 +83,6 @@ export default {
   },
 
   computed: {
-    //...mapState(["testdata", "ShopID", "orderType", "cart"]),
-    //테스트 데이터 추가 버전
     ...mapState([
       "testProduct",
       "testTag",
@@ -112,47 +94,7 @@ export default {
       "cart",
     ]),
 
-    tags() {
-      return this.$store.state.kioskModule.tags;
-    },
-    categories() {
-      return this.$store.state.kioskModule.categories;
-    },
-    options() {
-      return this.$store.state.kioskModule.options;
-    },
-    products() {
-      return this.$store.state.kioskModule.products;
-    },
-    tagMenu() {
-      return this.$store.state.kioskModule.tagMenu;
-    },
-
     filteredTagsByProductId() {
-      // db 사용 시
-      // return () => {
-      //   if (!this.selectedProduct) return { tags: [], options: [] }; // 선택된 상품이 없으면 빈 배열 반환
-
-      //   const productId = this.selectedProduct.id; // 선택된 상품의 ID 가져오기
-      //   const matchedTags = this.tagMenu.filter(tag => tag.productId === productId); // productId와 일치하는 tagMenu 찾기
-
-      //   const matchedTagIds = matchedTags.map(tag => tag.tagId); // 일치하는 tag의 tagId 추출
-      //   const filteredTags = this.tags.filter(tag => matchedTagIds.includes(tag.id)); // tagId와 일치하는 tag 필터링
-
-      //   console.log('옵션들:',this.options);
-      //   const matchedOptionIds = [];
-      //     matchedTagIds.forEach(tag => {
-      //       const options = this.options.filter(option => option.tag == tag);
-      //       matchedOptionIds.push(...options.map(option => option.id));
-      //     });
-      //     // 옵션 ID를 사용하여 해당 옵션들을 필터링합니다.
-      //     const filteredOptions = this.options.filter(option => matchedOptionIds.includes(option.id));
-      //     console.log("선택 상품의 태그들 현황: ",filteredTags);
-      //     console.log("선택 상품의 태그의 옵션들: ",filteredOptions);
-      //     //return { tags: matchedTags, options: filteredOptions };
-      //     return { tags: filteredTags, options: filteredOptions };
-
-      //testdata 사용 시
       return () => {
         if (!this.selectedProduct) return { tags: [], options: [] };
 
@@ -160,13 +102,11 @@ export default {
         const matchedTags = this.testTagMenu.filter(
           (tag) => tag.productId === productId
         );
-
         const matchedTagIds = matchedTags.map((tag) => tag.tagId);
         const filteredTags = this.testTag.filter((tag) =>
           matchedTagIds.includes(tag.id)
         );
 
-        console.log("옵션들:", this.options);
         const matchedOptionIds = [];
         matchedTagIds.forEach((tag) => {
           const options = this.testOption.filter((option) => option.tag == tag);
@@ -176,8 +116,6 @@ export default {
         const filteredOptions = this.testOption.filter((option) =>
           matchedOptionIds.includes(option.id)
         );
-        console.log("선택 상품의 태그들 현황: ", filteredTags);
-        console.log("선택 상품의 태그의 옵션들: ", filteredOptions);
         return { tags: filteredTags, options: filteredOptions };
       };
     },
@@ -196,15 +134,6 @@ export default {
     }
     this.playMenuAudio();
   },
-
-  // async created() {
-  //   // 데이터를 비동기적으로 로드
-  //   // this.$store.dispatch('fetchCategories');
-  //   // this.$store.dispatch('fetchTags')
-  //   // this.$store.dispatch('fetchOptions');
-  //   // this.$store.dispatch('fetchProducts');
-  //   // this.$store.dispatch('fetchTagMenu');
-  // },
 
   beforeUnmount() {
     window.removeEventListener("scroll", this.handleScroll);
@@ -260,16 +189,6 @@ export default {
     ]),
 
     handleScroll() {
-      //db연결 시
-      // for (let i = 0; i < this.categories.length; i++) {
-      //   const category = this.categories[i];
-      //   const element = document.getElementById('CategoryTitle' + categories.name);
-      //   if (window.scrollY >= element.offsetTop - 140) {
-      //     this.tab = i;
-      //   }
-      // }
-
-      // testdata 사용 시
       for (let i = 0; i < this.testCategory.length; i++) {
         const category = this.testCategory[i];
         const element = document.getElementById(
@@ -285,7 +204,6 @@ export default {
       this.stopAllAudio();
       this.playOptionAudio();
       this.selectedProduct = data;
-      console.log("선택 상품: ", this.selectedProduct);
       this.showOptionModal = true;
       this.showCartModal = false;
     },
@@ -305,10 +223,7 @@ export default {
       this.stopAllAudio();
       this.playAddOrderAudio();
       this.showOptionModal = false;
-      console.log("개수: ", $event.num);
-      console.log("가격:", $event.price);
       for (let i = 0; i < $event.num; i++) {
-        console.log("옵션:", $event.option);
         this.addCart({
           product: {
             name: this.selectedProduct.name,
@@ -342,15 +257,8 @@ export default {
       this.$router.push("/order-type/common");
     },
 
-    playClickSound() {
-      const clickSound = new Audio(require("@/assets/click-sound.mp3"));
-      clickSound.play().catch((error) => {
-        console.error("Error playing click sound:", error);
-      });
-    },
-
     getCategoryNameById(id) {
-      const category = this.categories.find(cat => cat.id === id);
+      const category = this.categories.find((cat) => cat.id === id);
       return category ? category.name : null;
     },
   },
